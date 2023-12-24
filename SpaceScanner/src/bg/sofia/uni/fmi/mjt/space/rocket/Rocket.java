61ca6a5685692042a6ad19068d9a8c834a8a9710
kostadinrusalov <rusalov@uni-sofia.bg>
2023-12-25 00:08:43 +0200

    Add parsing for mission, rocket, detail and statuses

--- SpaceScanner/src/bg/sofia/uni/fmi/mjt/space/rocket/Rocket.java
+++ SpaceScanner/src/bg/sofia/uni/fmi/mjt/space/rocket/Rocket.java
@@ -1,6 +1,25 @@
 package bg.sofia.uni.fmi.mjt.space.rocket;
 
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public record Rocket(String id, String name, Optional<String> wiki, Optional<Double> height) {
+    private static final String DELIMITER = ",";
+    private static final int ID = 0;
+    private static final int NAME = 1;
+    private static final int WIKI = 2;
+    private static final int HEIGHT = 3;
+
+    public static Rocket from(String line) {
+        String[] tokens = line.split(DELIMITER);
+
+        Optional<String> wiki = tokens.length <= WIKI || tokens[WIKI].isBlank() ?
+            Optional.empty() : Optional.of(tokens[WIKI]);
+
+        Optional<Double> height = tokens.length <= HEIGHT ? Optional.empty() :
+            Optional.of(Double.valueOf(tokens[HEIGHT].substring(0, tokens[HEIGHT].indexOf(' '))));
+
+        return new Rocket(tokens[ID], tokens[NAME], wiki, height);
+    }
 }
