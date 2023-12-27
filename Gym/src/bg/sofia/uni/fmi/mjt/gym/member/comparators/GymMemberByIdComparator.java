9cad8c656f28d4113b4a30f3fbb78089f05cdee6
kostadinrusalov <rusalov@uni-sofia.bg>
2023-12-27 21:00:09 +0200

    Add Gym task

---
+++ Gym/src/bg/sofia/uni/fmi/mjt/gym/member/comparators/GymMemberByIdComparator.java
@@ -0,0 +1,12 @@
+package bg.sofia.uni.fmi.mjt.gym.member.comparators;
+
+import bg.sofia.uni.fmi.mjt.gym.member.GymMember;
+
+import java.util.Comparator;
+
+public class GymMemberByIdComparator implements Comparator<GymMember> {
+    @Override
+    public int compare(GymMember o1, GymMember o2) {
+        return o1.getPersonalIdNumber().compareTo(o2.getPersonalIdNumber());
+    }
+}
