7419cf0cd4f0f7b1377ea35487723bbda0b63e9f
kostadinrusalov <rusalov@uni-sofia.bg>
2024-09-14 17:44:43 +0300

    remove old

--- Gym/src/bg/sofia/uni/fmi/mjt/gym/member/Address.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.gym.member;
-
-public record Address(double longitude, double latitude) {
-    public double getDistanceTo(Address other) {
-        return Math.hypot(other.longitude - longitude, other.latitude - latitude);
-    }
-}
--- T-ShirtShop/Server/src/bg/sofia/uni/fmi/mjt/order/server/repository/exception/OrderNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package bg.sofia.uni.fmi.mjt.order.server.repository.exception;
-
-public class OrderNotFoundException {
-}
