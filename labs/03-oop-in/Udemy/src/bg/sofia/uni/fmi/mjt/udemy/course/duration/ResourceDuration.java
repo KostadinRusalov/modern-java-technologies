050eb6315ed9a7d9baa84e29320bc6e4bc6a46ee
KostadinRusalov <rusalov@uni-sofia.bg>
2024-09-14 16:59:43 +0300

    Delete labs/03-oop-in/Udemy directory

--- labs/03-oop-in/Udemy/src/bg/sofia/uni/fmi/mjt/udemy/course/duration/ResourceDuration.java
+++
@@ -1,9 +0,0 @@
-package bg.sofia.uni.fmi.mjt.udemy.course.duration;
-
-public record ResourceDuration(int minutes) {
-    public ResourceDuration {
-        if (minutes < 0 || minutes > 60) {
-            throw new IllegalArgumentException("Minutes must be between 0 and 60");
-        }
-    }
-}
